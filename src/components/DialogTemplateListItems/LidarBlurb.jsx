9074e3fc6a6206bb6d94e837a364a34427c88c0a
jwhaney <john.haney@twdb.texas.gov>
2020-01-08 10:41:10 -0600

    clean up previous text commented out
    issue #243

--- src/components/DialogTemplateListItems/LidarBlurb.jsx
+++ src/components/DialogTemplateListItems/LidarBlurb.jsx
@@ -9,11 +9,6 @@ export default class LidarBlurb extends React.Component {
           About Lidar Data
         </div>
         <p>
-          {/*
-          Lidar data for Texas is made available online through the use of data compression using&nbsp;
-          <a href="https://rapidlasso.com/lastools/" target="_blank" rel="noopener noreferrer">LASTools</a>.
-          LASTools is an open-source collection of tools for lidar data viewing and manipulation.
-          */}
           Lidar data for Texas is available online through the use of <a href="https://rapidlasso.com/lastools/" target="_blank" rel="noopener noreferrer">LASTools</a>,&nbsp;
           an open-source collection of tools for lidar data viewing and manipulation.
         </p>
