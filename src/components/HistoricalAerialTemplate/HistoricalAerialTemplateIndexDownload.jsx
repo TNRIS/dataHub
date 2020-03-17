0ad2b774947b0a1b30aeb3e267d686e9df9f2ada
Marlboro Blend No. 27 <adam.breznicky@twdb.texas.gov>
2020-03-17 15:42:33 -0500

    working edits

--- src/components/HistoricalAerialTemplate/HistoricalAerialTemplateIndexDownload.jsx
+++ src/components/HistoricalAerialTemplate/HistoricalAerialTemplateIndexDownload.jsx
@@ -24,7 +24,8 @@ export default class HistoricalAerialTemplateIndexDownload extends React.Compone
             container: 'historical-index-download-map', // container id
             style: 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json',
             center: [-99.341389, 31.330000],
-            zoom: 4
+            zoom: 4,
+            minZoom: 5
         });
         this.map = map;
         // add regular out-of-the-box controls if they dont already exist
@@ -56,41 +57,72 @@ export default class HistoricalAerialTemplateIndexDownload extends React.Compone
         const rasterLayer = this.props.serviceLayer + '_index';
         const boundaryLayer = this.props.serviceLayer + '_index_index';
         const baseUrl = this.props.indexUrl + '&bbox={bbox-epsg-3857}&format=image/png&service=WMS&version=1.1.1&request=GetMap&srs=EPSG:3857&transparent=true&width=256&height=256&layers=';
+        // const filler = this.props.theme + "Fill";
+        // const texter = this.props.theme + "Text";
+        const filler = "lightFill";
+        const texter = "lightText";
         map.on('load', function() {
             // create sources, add layers
             // raster layer
             map.addSource('index-raster-wms', {
-                'type': 'raster',
-                'tiles': [
+                type: 'raster',
+                tiles: [
                     baseUrl + rasterLayer
                 ],
-                'tileSize': 256
+                tileSize: 256
             });
             map.addLayer(
                 {
-                'id': 'raster-layer',
-                'type': 'raster',
-                'source': 'index-raster-wms',
-                'paint': {}
+                    id: 'raster-layer',
+                    type: 'raster',
+                    source: 'index-raster-wms',
+                    paint: {}
                 }
             );
             // boundary layer
             map.addSource('index-boundary-wms', {
-                'type': 'raster',
-                'tiles': [
+                type: 'raster',
+                tiles: [
                     baseUrl + boundaryLayer
                 ],
-                'tileSize': 256
+                tileSize: 256
             });
             map.addLayer(
                 {
-                'id': 'boundary-layer',
-                'type': 'raster',
-                'source': 'index-boundary-wms',
-                'paint': {}
+                    id: 'boundary-layer',
+                    type: 'raster',
+                    source: 'index-boundary-wms',
+                    paint: {}
                 }
             );
-            
+        });
+        // popup
+        map.on('click', 'boundary-layer', function (e) {
+            console.log(e);
+            console.log(e.features);
+        // const clickedAreaId = e.features[0].properties.area_type_id;
+        // const clickedAreaName = e.features[0].properties.area_type_name;
+        // const downloads = areaLookup[clickedAreaId];
+        // let popupContent = "";
+        // // iterate available downloads for the area
+        // Object.keys(downloads).sort().map(abbr => {
+        //   const dldInfo = downloads[abbr];
+        //   // if a filesize is populated in the resource table so the popup,
+        //   // we don't want to display empty popups, right?
+        //   let filesizeString = "";
+        //   if (dldInfo.filesize != null) {
+        //     const filesize = parseFloat(dldInfo.filesize / 1000000).toFixed(2).toString();
+        //     filesizeString = " - " + filesize + "MB";
+        //   }
+        //   // create html link and append to content string
+        //   const dld = `<li><a href="${dldInfo.link}" target="_blank">${dldInfo.name}${filesizeString}</a></li>`;
+        //   return popupContent += dld;
+        // });
+        // // create popup with constructed content string
+        // new mapboxgl.Popup()
+        //   .setLngLat(e.lngLat)
+        //   .setHTML(`<strong>${clickedAreaName}</strong><ul>${popupContent}</ul>`)
+        //   .addTo(map);
         });
     }
   
